ff7abd12540c050b30bed4f3fd1cca2cf22f6d0e
Ketaro Takeuchi <tiyotiyouda@gmail.com>
2014-12-01 21:23:05 +0900

    add core.async

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main raspberrypi.core
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.twitter4j/twitter4j-core "[4.0,)"]
                  [org.twitter4j/twitter4j-stream"[4.0,)"]])
